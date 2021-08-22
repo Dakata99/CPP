db7df53c539fecb27bea64f5d9c9ca0d44cba64a
Daniel Lyubenov <44753174+Dakata99@users.noreply.github.com>
2021-08-22 12:15:13 +0300

    Delete library directory

--- library/Strlib.cpp
+++
@@ -1,8 +0,0 @@
-#include "Strlib.h"
-
-bool is_number(const std::string& num)
-{
-    std::regex double_num("^(-|)[0-9]+(\\.[0-9]+)?$");
-
-    return std::regex_match(num, double_num);
-}
--- library/Strlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRLIB_H_
-#define _STRLIB_H_
-
-#include <regex>
-#include <string>
-
-bool is_number(const std::string&);
-
-#endif
